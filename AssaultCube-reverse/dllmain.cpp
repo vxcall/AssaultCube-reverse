6f1211b86c039007bad8d9b0a550de43a3058249
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-09-25 22:49:15 +0800

    Initial commit
    just a base

---
+++ AssaultCube-reverse/dllmain.cpp
@@ -0,0 +1,42 @@
+#include <Windows.h>
+#include <iostream>
+
+void Detach() {
+    fclose(stdout);
+    fclose(stderr);
+    FreeConsole();
+}
+
+
+DWORD WINAPI fMain(LPVOID lpParameter) {
+    AllocConsole();
+    FILE* fp = NULL;
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+    freopen_s(&fp, "CONOUT$", "w", stderr);
+
+    while(true) {
+        if (GetAsyncKeyState(VK_DELETE) & 1) {
+            break;
+        }
+    }
+
+    FreeLibraryAndExitThread(static_cast<HMODULE>(lpParameter), EXIT_SUCCESS);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hModule);
+
+        HANDLE hThread = CreateThread(nullptr, 0, fMain, hModule, 0, nullptr);
+        if (hThread)
+        {
+            CloseHandle(hThread);
+        }
+    } else if (dwReason == DLL_PROCESS_DETACH && !lpReserved)
+    {
+        Detach();
+    }
+    return TRUE;
+}
