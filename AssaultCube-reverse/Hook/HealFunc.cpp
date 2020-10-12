988be651e1a9d4c9096949bf649a7e9fb824e12c
tomsa <45645938+tomsa000@users.noreply.github.com>
2020-10-12 15:00:27 +0300

    Update HealFunc.cpp

--- AssaultCube-reverse/Hook/HealFunc.cpp
+++ AssaultCube-reverse/Hook/HealFunc.cpp
@@ -3,24 +3,25 @@
 #include <iostream>
 #include "detours.h"
 
-using tHeal = int (__stdcall*) (void* result, int a2);
+using tHeal = void* (__stdcall*) (void*, int);
 tHeal oHeal = nullptr;
 
-int __stdcall hHeal(void* result, int a2) { //result@<eax>, a2@<ecx>
+void* __stdcall hHeal(void* player, int a2) { //result@<eax>, a2@<ecx>
     std::cout << "healed" << std::endl;
-    return oHeal(result, a2);
+    return oHeal(player, a2);
 }
 
-__declspec(naked) void __stdcall func_hook(void* result, int a2) {
-    __asm{
+__declspec(naked) void __stdcall func_hook(void* player, int a2) 
+{
+  __asm
+  {
     push ebp
     mov ebp, esp
     push dword ptr[a2]
-    push dword ptr[result]
+    push dword ptr[player]
     call hHeal
     retn 04h
-    }
-}
+  }
 
 void hookHeal() {
     oHeal = reinterpret_cast<tHeal>(DetourFunction(reinterpret_cast<PBYTE>(0x0044672F), reinterpret_cast<PBYTE>(func_hook)));
